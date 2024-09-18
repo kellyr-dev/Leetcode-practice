15cc1d035644822a6663778e0d178f2a8c184e03
KellyR <kellyruiz.dev@gmail.com>
2024-09-17 21:40:56 -0700

    subsets

--- subsets.py
+++ subsets.py
@@ -45,7 +45,47 @@ def subsetsWithDup(nums):
 
     return result
 
+# 784. Letter Case Permutation
+def findLetterCaseStringPermutations(letters):
+
+    aux_result = [[]]
+
+    for i in range(len(letters)):
+        n = len(aux_result)
+        for j in range(n):
+
+            if letters[i].isnumeric():
+                aux_result[j].append(letters[i])
+            else:
+                letterCap = ""
+                if letters[i].islower():
+                    letterCap += letters[i].capitalize()
+                else:
+                    letterCap += letters[i].lower()
+
+                letter = letters[i]
+
+                aux1 = list(aux_result[j])
+                aux2 = list(aux_result[j])
+                aux1.append(letterCap)
+                aux2.append(letter)
+
+                aux_result[j] = aux1
+                aux_result.append(aux2)
+
+    print(aux_result)
+
+    result = []
+
+    for array_letter in aux_result:
+        aux = "".join(array_letter)
+        result.insert(0,aux)
+
+    return result
+
+
 if __name__ == '__main__':
 
-    nums = [1, 2, 2]
-    print(subsetsWithDup(nums))
+    nums = [1, 3, 5]
+    letters = "ad52"
+    print(findLetterCaseStringPermutations(letters))
