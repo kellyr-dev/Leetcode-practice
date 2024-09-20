342240f3743ddedf23ff6c72bc26c4e0b0008ec2
KellyR <kellyruiz.dev@gmail.com>
2024-09-19 21:36:45 -0700

    balanced parenthesis

--- subsets.py
+++ subsets.py
@@ -83,9 +83,66 @@ def findLetterCaseStringPermutations(letters):
 
     return result
 
+#22. Generate Parentheses
+def generateParenthesis(n):
+
+    if n == 1:
+        return ["()"]
+    if n == 2:
+        return ["(())", "()()"]
+
+    newn = n*2
+    result = ['(']
+    table_hash = {'(': (1,0)}
+    for i in range(newn):
+        lenght = len(result)
+        for j in range(lenght):
+
+            auxOpen = table_hash[result[j]][0]
+            auxClose = table_hash[result[j]][1]
+            strinCase = result[j]
+            #table_hash.pop(strinCase)
+
+            if auxOpen < n:
+                if auxOpen == auxClose:
+                    # only can add '('
+                    strinCase += '('
+                    key = (auxOpen+1, auxClose)
+                    result[j] = strinCase
+                    table_hash[strinCase] = key
+                elif auxOpen > auxClose:
+                    # must be the normal case add '(' and ')'
+                    auxStrOpen = strinCase + '('
+                    auxStrClose = strinCase + ')'
+                    keyOpen = (auxOpen + 1, auxClose)
+                    keyClose = (auxOpen, auxClose + 1)
+
+                    table_hash[auxStrOpen] = keyOpen
+                    table_hash[auxStrClose] = keyClose
+
+                    result[j] = auxStrOpen
+                    result.append(auxStrClose)
+
+            elif auxOpen == n:
+                if auxOpen == auxClose:
+                    # continue is ok
+                    continue
+                    #key = (auxOpen, auxClose)
+                    #table_hash[strinCase] = key
+                elif auxClose < auxOpen:
+                    # only can add ')'
+                    strinCase += ')'
+                    result[j] = strinCase
+                    key = (auxOpen, auxClose + 1)
+                    table_hash[strinCase] = key
+
+    return result
+
+
 
 if __name__ == '__main__':
 
     nums = [1, 3, 5]
     letters = "ad52"
-    print(findLetterCaseStringPermutations(letters))
+    n = 3
+    print(generateParenthesis(n))
