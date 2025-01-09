1b8470bba94a8c83c4505459625a4db4a7d0a3e9
KellyR <kellyruiz.dev@gmail.com>
2025-01-08 21:44:14 -0800

    snap

---
+++ snap.py
@@ -0,0 +1,11 @@
+import heapq
+
+# 527. Word Abbreviation
+def wordsAbbreviation(words):
+
+    return ""
+
+if __name__ == '__main__':
+
+    words = ["like","god","internal","me","internet","interval","intension","face","intrusion"]
+    print(wordsAbbreviation(words))
