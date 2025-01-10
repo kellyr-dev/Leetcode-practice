5a2e3e58cf5c46b21793146ba284021aae3d1267
KellyR <kellyruiz.dev@gmail.com>
2025-01-09 21:09:40 -0800

    snap

--- snap.py
+++ snap.py
@@ -1,11 +1,23 @@
 import heapq
 
-# 527. Word Abbreviation
-def wordsAbbreviation(words):
+# 151. Reverse Words in a String
+def reverseWords(s):
 
-    return ""
+    auxWords = s.split(" ")
+    print(auxWords)
+    resultWords = []
+    for word in auxWords:
+
+        current = word.strip()
+        print(f"current: {current}")
+        if current != "" and current != " ":
+            resultWords.insert(0, current)
+
+    return " ".join(list(resultWords))
+
 
 if __name__ == '__main__':
 
     words = ["like","god","internal","me","internet","interval","intension","face","intrusion"]
-    print(wordsAbbreviation(words))
+    s = "the sky is blue"
+    print(reverseWords(s))
